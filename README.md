cf980e4ea7075a381828fd59f17646f10bda51cb
sabaemi <saba.em9@gmail.com>
2022-11-01 15:29:32 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Locking is one of the methods of preventing the problems of producing incorrect results by executing transactions in the database. Unfortunately, locking can lead to a deadlock, and every deadlock must be detected and resolved. One way to resolve any deadlock is to break the waiting loop, which fortunately has the possibility of rollback in the database, which has its disadvantages far less than killing the transaction. 
+In this program, it is assumed that two transactions are executed by two threads at the same time and use a common database. For simplicity, the following assumptions are made:
+1. The database consists of only one table that has only 100 records.
+2. There is only one type of lock in the system, which is X-lock with the same standard protocol of this lock. Therefore, the same exclusive lock must be used even for reading records. These threads accidentally (using the ransom number generator) lock the record number they need and do it so often that a deadlock occurs. The task of the deadlock detection routine is to detect the deadlock and Fix it. Once we have correctly identified the deadlock and are sure that the program is working properly, we just exit the program.
+Notes:
+1. We have two parallel threads
+2. To determine which records are locked by which transaction, we mention the name of the transaction in front of each record.
+3. We use the database (here) and communicate with it.
+4. The deadlock detection part is allowed to read any record in any order and at any time without needing to lock it.
---
+++ program/AdvancedDatabaseHW2/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace AdvancedDatabaseHW2
+{
+    public class Program
+    {
+        public static Thread T1, T2, T3;
+        public void thread1()
+        {
+            while (true)
+            {
+                Record C = new Record(1);
+                RecordManager recordManagerB = new RecordManager(C, C);
+                recordManagerB.Detect();
+                // Sleep for 4 seconds
+                Thread.Sleep(4000);
+            }
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Main Started");
+            while (true)
+            {
+                Random rd = new Random();
+                Record A = new Record(rd.Next(1, 10));
+                Record B = new Record(rd.Next(1, 10));
+
+                Record C = new Record(rd.Next(1, 10));
+                Record D = new Record(rd.Next(1, 10));
+
+                RecordManager recordManagerA = new RecordManager(A, B);
+                T1 = new Thread(recordManagerA.Transfer);
+                T1.Name = "1";
+
+                RecordManager recordManagerB = new RecordManager(C, D);
+                T2 = new Thread(recordManagerB.Transfer);
+                T2.Name = "2";
+
+                Program obj = new Program();
+                T3 = new Thread(obj.thread1);
+                T3.Name = "3";
+
+                T1.Start();
+                T2.Start();
+                T3.Start();
+
+                T1.Join();
+                T2.Join();
+
+                Console.WriteLine("Main Completed");
+            }
+        }
+    }
+
+    public class Record
+    {
+        int _id;
+
+        public Record(int id)
+        {
+            this._id = id;
+        }
+
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+        }
+    }
+
+    public class RecordManager
+    {
+        Record _first;
+        Record _second;
+
+        static int cntr;
+
+        public RecordManager(Record frst, Record scnd)
+        {
+            this._first = frst;
+            this._second = scnd;
+        }
+
+        public void Transfer()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+              "Data Source=DESKTOP-ADGLMB7;" +
+              "Initial Catalog=myDatabase;" +
+              "Integrated Security=true;";
+            conn.Open();
+            StringBuilder sb = new StringBuilder();
+            string sql = sb.ToString();
+            using var cmnd = new SqlCommand();
+            cmnd.Connection = conn;
+            int a = 0, b = 0;
+            int f = 0;
+            while (f == 0)
+            {
+                sql = "SELECT ID FROM htw WHERE ID = " + _first.ID + " AND Lock !=" + ((5 - Int32.Parse(Thread.CurrentThread.Name)) / 2) + " ;";
+                using (SqlCommand command2 = new SqlCommand(sql, conn))
+                {
+                    using (SqlDataReader reader2 = command2.ExecuteReader())
+                    {
+                        while (reader2.Read())
+                        {
+                            a = reader2.GetInt32(0);
+                        }
+                    }
+                }
+                if (a == _first.ID)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " trying to acquire lock on " + _first.ID.ToString());
+                    lock (_first)
+                    {
+                        cmnd.CommandText = "UPDATE htw SET Lock = " + Int32.Parse(Thread.CurrentThread.Name) + " WHERE ID = " + _first.ID + ";";
+                        cmnd.ExecuteNonQuery();
+                        Console.WriteLine(Thread.CurrentThread.Name + " acquires lock on " + _first.ID);
+                        f = 1;
+                        Console.WriteLine(Thread.CurrentThread.Name + " suspended for 1 second");
+                        Thread.Sleep(1000);
+                        Console.WriteLine(Thread.CurrentThread.Name + " back in action and trying to acquire lock on " + _second.ID.ToString());
+                        f = 0;
+                        while (f == 0)
+                        {
+                            cntr++;
+                            Detect();
+                            sql = "SELECT ID FROM htw WHERE ID = " + _second.ID + " AND Lock !=" + ((5 - Int32.Parse(Thread.CurrentThread.Name)) / 2) + " ;";
+                            using (SqlCommand command2 = new SqlCommand(sql, conn))
+                            {
+                                using (SqlDataReader reader2 = command2.ExecuteReader())
+                                {
+                                    while (reader2.Read())
+                                    {
+                                        b = reader2.GetInt32(0);
+                                    }
+                                }
+                            }
+                            if (b == _second.ID)
+                            {
+                                lock (_second)
+                                {
+                                    cntr = 0;
+                                    cmnd.CommandText = "UPDATE htw SET Lock = " + Int32.Parse(Thread.CurrentThread.Name) + " WHERE ID = " + _second.ID + ";";
+                                    cmnd.ExecuteNonQuery();
+                                    Console.WriteLine(Thread.CurrentThread.Name + " acquires lock on " + _second.ID);
+                                    f = 1;
+                                    Console.WriteLine(Thread.CurrentThread.Name + " suspended for 1 second");
+                                    cmnd.CommandText = "UPDATE htw SET Lock = 0 WHERE Lock = " + Int32.Parse(Thread.CurrentThread.Name) + ";";
+                                    cmnd.ExecuteNonQuery();
+                                    Console.WriteLine("Thread " + Thread.CurrentThread.Name + " released all records ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public void Detect()
+        {
+            if (cntr > 12000)
+            {
+                cntr = 0;
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString =
+                  "Data Source=DESKTOP-ADGLMB7;" +
+                  "Initial Catalog=myDatabase;" +
+                  "Integrated Security=true;";
+                conn.Open();
+                using var cmnd = new SqlCommand();
+                cmnd.Connection = conn;
+                int a = 0, b = 0;
+                StringBuilder sb = new StringBuilder();
+                string sql = sb.ToString();
+
+                Console.WriteLine("**** DeadLock Detected!");
+                sql = "SELECT ID FROM htw WHERE Lock =1 ;";
+                using (SqlCommand command2 = new SqlCommand(sql, conn))
+                {
+                    using (SqlDataReader reader2 = command2.ExecuteReader())
+                    {
+                        while (reader2.Read())
+                        {
+                            a = reader2.GetInt32(0);
+                            Console.WriteLine("Record " + a + " is locked by thread 1");
+                        }
+                    }
+                }
+                sql = "SELECT ID FROM htw WHERE Lock =2 ;";
+                using (SqlCommand command2 = new SqlCommand(sql, conn))
+                {
+                    using (SqlDataReader reader2 = command2.ExecuteReader())
+                    {
+                        while (reader2.Read())
+                        {
+                            b = reader2.GetInt32(0);
+                            Console.WriteLine("Record " + b + " is locked by thread 2");
+                        }
+                    }
+                }
+
+                Console.WriteLine("Thread 1 is waiting for thread 2 to release lock on record " + b);
+                Console.WriteLine("Thread 2 is waiting for thread 1 to release lock on record " + a);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
